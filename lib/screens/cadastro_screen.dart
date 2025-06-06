81a2832002e709bdd1d97a0270e5a0ed6f7a7007
Kennedy Viana <80414538+kennedyyvianna@users.noreply.github.com>
2025-06-06 11:01:14 -0300

    atualizacao

--- lib/screens/cadastro_screen.dart
+++ lib/screens/cadastro_screen.dart
@@ -9,12 +9,12 @@ class CadastroScreen extends StatefulWidget{
 }
 
 class _CadastroScreenState extends State<CadastroScreen> {
-  final TextEditingController nomeController = TextEditingController();
+  final TextEditingController nameController = TextEditingController();
   final TextEditingController emailController = TextEditingController();
-  final TextEditingController senhaController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
 
   Future<void> salvarUsuario() async {
-    if (nomeController.text.isEmpty || emailController.text.isEmpty || senhaController.text.isEmpty) {
+    if (nameController.text.isEmpty || emailController.text.isEmpty || passwordController.text.isEmpty) {
       ScaffoldMessenger.of(
         context,
       ).showSnackBar(const SnackBar(content: Text('Preencha todos os campos!')));
@@ -22,20 +22,132 @@ class _CadastroScreenState extends State<CadastroScreen> {
     }
 
     await FirebaseFirestore.instance.collection('cadastros').add({
-      'nome': nomeController.text,
+      'nome': nameController.text,
       'email': emailController.text,
-      'senha': senhaController.text,
+      'senha': passwordController.text,
     });
 
     ScaffoldMessenger.of(
       context,
     ).showSnackBar(const SnackBar(content: Text('Cadastro salvo com sucesso!')));
 
-    nomeController.clear();
+    nameController.clear();
     emailController.clear();
-    senhaController.clear();
+    passwordController.clear();
 
-    Navigator.pushNamed(context, '/listar');
+    // Navigator.pushNamed(context, '/listar');
   }
 
+@override
+  Widget build(BuildContext context) {
+    final Color laranjaForte = const Color(0xFFF67828);
+
+    return Scaffold(
+      appBar: AppBar(title: const Text('Cadastro de Usuário')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Container(
+                padding: const EdgeInsets.all(24),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20),
+                  border: Border.all(color: laranjaForte, width: 2),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black12,
+                      blurRadius: 8,
+                      offset: const Offset(0, 4),
+                    )
+                  ],
+                ),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      'Login',
+                      style: TextStyle(
+                        color: laranjaForte,
+                        fontSize: 28,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 24),
+                    TextFormField(
+                      controller: nameController,
+                      decoration: InputDecoration(
+                        labelText: 'Nome',
+                        labelStyle: TextStyle(color: laranjaForte),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: laranjaForte),
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 16),
+                    TextFormField(
+                      controller: emailController,
+                      decoration: InputDecoration(
+                        labelText: 'Email',
+                        labelStyle: TextStyle(color: laranjaForte),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: laranjaForte),
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 16),
+                    TextFormField(
+                      controller: passwordController,
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        labelText: 'password',
+                        labelStyle: TextStyle(color: laranjaForte),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: laranjaForte),
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 24),
+                    SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          // Navigator.pushReplacementNamed(context, AppRoutes.espacos);
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: laranjaForte,
+                          padding: const EdgeInsets.symmetric(vertical: 16),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                        ),
+                        child: const Text(
+                          'Cadastrar',
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+          ],
+        ),
+      ),
+    );
+  }
 }
